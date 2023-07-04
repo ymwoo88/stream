4bb93e418f278a9576392e044212076b4f37f833
우영민 <ymwoo@hanatour.com>
2023-07-04 19:09:09 +0900

    find기능 예제
    ## 각각 기능 예제 사용기
    - findAny
    - findFirst

---
+++ src/test/java/com/ymwoo/stream/first/StreamFirstSeriesTest.java
@@ -0,0 +1,38 @@
+package com.ymwoo.stream.first;
+
+import com.ymwoo.stream.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+public class StreamFirstSeriesTest {
+
+    @Test
+    @DisplayName("Stream findAny, findFirst 테스트")
+    void case1() {
+        List<User> examUserList = User.getExamUserList();
+
+        Optional<User> first = examUserList.stream()
+                .filter(User::getIsWorking)
+                .findFirst(); // return 이 Optional
+
+        List<User> isWorkingUserList = examUserList.stream()
+                .filter(User::getIsWorking).toList();
+        // 2개 이상 검증
+        Assertions.assertEquals(2, isWorkingUserList.size());
+        // 2개 중에 any 뭐가 나올까..?
+        Optional<User> any = isWorkingUserList.stream()
+                .filter(User::getIsWorking)
+                .findAny(); // return 이 Optional
+
+        // 확인
+        log.info("findFirst = {}", first.get());
+        // any 결과를 보니까 first와 다를게 없어보이네.. 무조건 앞에께 튀나오네
+        log.info("findAny = {}", any.get());
+    }
+}
